eb14c1a028749deafd4c825655f76250ff8460b3
gz-adrian <102628359+gz-adrian@users.noreply.github.com>
2022-07-03 19:44:23 -0700

    mini fixed

--- ram.js
+++ ram.js
@@ -5,7 +5,7 @@
 //     this.currentOperandTextElement = currentOperandTextElement
 //     this.clear()
 //   }
-//more notes 
+//more notes
 //   clear() {
 //     this.currentOperand = ''
 //     this.previousOperand = ''
@@ -87,28 +87,4 @@
 // }
 
 
-const numberButtons = document.querySelectorAll('[data-number]')
-const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelector('[data-equals]')
-const deleteButton = document.querySelector('[data-delete]')
-const allClearButton = document.querySelector('[data-all-clear]')
-const previousOperandTextElement = document.querySelector('[data-previous-operand]')
-const currentOperandTextElement = document.querySelector('[data-current-operand]')
-
-const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
-
-// Notes
-// numberButtons.forEach(button => {
-//   button.addEventListener('click', () => {
-//     calculator.appendNumber(button.innerText)
-//     calculator.updateDisplay()
-//   })
-// })
-
-// operationButtons.forEach(button => {
-//   button.addEventListener('click', () => {
-//     calculator.chooseOperation(button.innerText)
-//     calculator.updateDisplay()
-//   })
-// })
-
+/// notes later
